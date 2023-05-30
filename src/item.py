64d300a71ec52a558047e2b7b40306530569a00c
svetlana romanenko <svroman07@gmail.com>
2023-05-31 01:28:36 +0300

    add classmethod&&staticmethod

--- src/item.py
+++ src/item.py
@@ -1,3 +1,5 @@
+from csv import DictReader
+from os import path
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +15,22 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name #делаем атрибут приватным
         self.price = price
         self.quantiti = quantity
         Item.all.append(self)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+
+    def name(self, new_name: str) -> None:
+        if len(new_name) <= 10:
+            self.__name = new_name
+        else:
+            print("Exception: Длина наименования товара превышает 10 символов.")
 
 
     def calculate_total_price(self) -> float:
@@ -32,3 +46,20 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price = self.price * self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        '''Класс-метод, инициализирующий экземпляры класса `Item`данными из файла ../src/items.csv'''
+        cls.all.clear()
+        with open(path.join('..', 'src', 'items.csv'), 'r', encoding='cp1251') as csvfile:
+            data = DictReader(csvfile)
+            for i in data:
+                cls(i['name'], i['price'], i['quantity'])
+
+    @staticmethod
+
+    def string_to_number(number):
+        '''Cтатический метод, возвращающий число из числа-строки'''
+        return int(float(number))
+
+
