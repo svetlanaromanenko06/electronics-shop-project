a51ac3c551aeb95652267b56a453cd47bd2beb0b
svetlana romanenko <svroman07@gmail.com>
2023-06-03 00:30:46 +0300

    add __repr__&&__str__ fix guantity

--- src/item.py
+++ src/item.py
@@ -17,8 +17,14 @@ class Item:
         """
         self.__name = name #делаем атрибут приватным
         self.price = price
-        self.quantiti = quantity
+        self.quantity = quantity
         Item.all.append(self)
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.__name}', {self.price}, {self.quantity})"
+
+    def __str__(self):
+        return f'{self.name}'
 
     @property
     def name(self):
