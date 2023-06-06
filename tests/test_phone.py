abf1b159472df929a9b1232f7ea9a72e443be761
svetlana romanenko <svroman07@gmail.com>
2023-06-06 22:23:34 +0300

    add test_phone

---
+++ tests/test_phone.py
@@ -0,0 +1,19 @@
+from src.phone import Phone
+import pytest
+
+@pytest.fixture
+def test_phone():
+    return Phone('iPhone 14', 120_000, 5, 2)
+
+def test_repr(test_phone):
+    assert repr(test_phone) == "Phone('iPhone 14', 120000, 5, 2)"
+
+def test_phone_init(test_phone):
+    assert test_phone.number_of_sim == 2
+    assert test_phone.name == 'iPhone 14'
+    assert test_phone.price == 120000
+    assert test_phone.quantity == 5
+
+
+
+
