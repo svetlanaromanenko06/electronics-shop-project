48839b3d9df315be44c823d8bd6535f18f56c741
svetlana romanenko <svroman07@gmail.com>
2023-06-08 21:34:47 +0300

    add setter

--- src/phone.py
+++ src/phone.py
@@ -6,10 +6,19 @@ class Phone(Item):
         дополнительно атрибут, содержащий количество поддерживаемых сим-карт"""
     def __init__(self, name: str, price: float, quantity: int, number_of_sim: int):
         super().__init__(name, price, quantity)
-        if number_of_sim > 0:
-            self.number_of_sim = number_of_sim
+
+    @property
+    def number_of_sim(self):
+        return self.number_of_sim
+
+
+    @number_of_sim.setter
+    def number_of_sim(self, num_sim):
+        if num_sim > 0:
+            self.number_of_sim = num_sim
         else:
             raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля')
+
 
     def __repr__(self):
         return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
